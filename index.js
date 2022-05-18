30ec6f41e960f273f47b24c7273ee6737929dce7
kaisa <kaisa.eronen@aalto.fi>
2022-05-18 10:33:05 +0300

    frontend added

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors())
 app.use(express.json())
 morgan.token('body', (request, reponse) => { return JSON.stringify(request.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(express.static('build'))
 
 
 app.get("/api/persons", (req, res) => {
